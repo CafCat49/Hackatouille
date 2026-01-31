c5431b5f7f0b6fb9ef9bc86d92280f2ad26de701
Christina A. H. <47138990+CafCat49@users.noreply.github.com>
2026-01-31 01:45:50 +0100

    Update Interactable.cs

--- Museum Heist/Assets/Scripts/Interactable.cs
+++ Museum Heist/Assets/Scripts/Interactable.cs
@@ -3,8 +3,21 @@ using UnityEngine.EventSystems;
 
 public class Interactable : MonoBehaviour, IPointerDownHandler
 {
+    bool activated = false;
+    
     public void OnPointerDown(PointerEventData eventData)
     {
         Debug.Log("I have been clicked OwO");
+        
+        if (!activated)
+        {
+            GetComponent<SpriteRenderer>().color = Color.green;
+            activated = true;
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().color = Color.red;
+            activated = false;
+        }
     }
 }

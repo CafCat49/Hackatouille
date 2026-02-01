3eedbdb96dae7e9d93c38140188d130ed46571b5
Christina A. H. <47138990+CafCat49@users.noreply.github.com>
2026-02-01 05:33:44 +0100

    A bit more cleanup

--- Museum Heist/Assets/Scripts/Interactable.cs
+++ Museum Heist/Assets/Scripts/Interactable.cs
@@ -2,12 +2,14 @@ using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Interactable : MonoBehaviour, IPointerDownHandler
+public class Interactable : MonoBehaviour, IPointerDownHandler, IPointerEnterHandler, IPointerExitHandler
 {
     bool activated = false;
     public string InteractionType = "";
     public static Action<string> OnActivate;
     public static Action<string> OnDeactivate;
+    public static Action OnMouseOverlap;
+    public static Action OnMouseEndOverlap;
     
     public void OnPointerDown(PointerEventData eventData)
     {
@@ -23,6 +25,22 @@ public class Interactable : MonoBehaviour, IPointerDownHandler
         {
             activated = false;
             OnDeactivate?.Invoke(InteractionType);
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (InteractionType == "Computer")
+        {
+            OnMouseOverlap?.Invoke();
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (InteractionType == "Computer")
+        {
+            OnMouseEndOverlap?.Invoke();
         }
     }
 }
--- Museum Heist/Assets/Scripts/Player.cs
+++ Museum Heist/Assets/Scripts/Player.cs
@@ -1,17 +1,21 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
-    public GameObject securityCam;
-    private SpriteRenderer securityCamRenderer;
+    public GameObject securityCam, computer;
+    private SpriteRenderer securityCamRenderer, computerRenderer;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         securityCamRenderer = securityCam.GetComponent<SpriteRenderer>();
+        computerRenderer = computer.GetComponent<SpriteRenderer>();
         Interactable.OnActivate += OnInteracted;
         Interactable.OnDeactivate += OnDeactivated;
+        Interactable.OnMouseOverlap += OnMouseHover;
+        Interactable.OnMouseEndOverlap += OnMouseEndHover;
     }
 
     private void OnInteracted(string interactionType)
@@ -42,9 +46,29 @@ public class Player : MonoBehaviour
         }
     }
 
+    private void OnMouseHover()
+    {
+        if (!computerRenderer) return;
+        
+        var color = computerRenderer.color;
+        color.a = 1;
+        computerRenderer.color = color;
+    }
+
+    private void OnMouseEndHover()
+    {
+        if (!computerRenderer) return;
+        
+        var color = computerRenderer.color;
+        color.a = 0.2f;
+        computerRenderer.color = color;
+    }
+
     void OnDestroy()
     {
         Interactable.OnActivate -= OnInteracted;
         Interactable.OnDeactivate -= OnDeactivated;
+        Interactable.OnMouseOverlap -= OnMouseHover;
+        Interactable.OnMouseEndOverlap -= OnMouseEndHover;
     }
 }
